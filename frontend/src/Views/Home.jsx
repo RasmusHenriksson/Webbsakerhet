ec98550e1f979d0a2e55c11cd4a471f7298243a7
Rasmus Henriksson <turboralle@hotmail.com>
2022-11-17 15:12:33 +0100

    :recycle: rafactored code

--- frontend/src/Views/Home.jsx
+++ frontend/src/Views/Home.jsx
@@ -10,10 +10,16 @@ const Home = () => {
   const [comments] = useState([
     {
       id: 1,
-      title: "Titel",
-      body: "Här kommer texten",
+      title: "Internship",
+      body: "<i> This is the first few weeks of my internship at google and I wish to tell all of you how grateful I am for this opportunity that i am now able to share with you all!",
       imgUrl:
-        "https://webbtelescope.org/files/live/sites/webb/files/home/news/first-images/gallery/_images/STScI-J-HeroERO-Gallery-Fallback-image-1920x1080.jpg",
+        "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Chrome__logo.max-500x500.png",
+    },
+    {
+      id: 1,
+      title: "Todays weather",
+      body: "<b> The weather today is outstanding! The sun is shining and the temperature keeps rising.</b> <br> Hope you all have a wonderful day out in the sun!",
+      imgUrl:"https://play-lh.googleusercontent.com/pCQw51XRP4UPr-FCYDjvNnEpFa0HDGJjjLDldN3rmw4KkwhqPu0PZXE8EopmAxzH9mQ",
     },
   ]);
 
