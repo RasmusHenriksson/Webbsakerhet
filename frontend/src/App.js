4db31680202f1f59c2a4dca89684085c046d6212
Fleeh <turboralle@hotmail.com>
2022-11-17 14:17:39 +0100

    :sparkles: Added backend-API in project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,26 @@ import Navbar from "./components/Navbar";
 import { Routes, Route } from 'react-router-dom';
 import Home from "./Views/Home";
 import Blogg from "./Views/Blogg";
+import { useEffect } from "react";
 
 
 
 
 const App = () => {
+
+  useEffect(() => {
+    fetch('https://localhost:7269/api/First/test1').then(res => {
+  console.log(res)
+  return res.json()
+})
+.then(data => {
+  console.log(data)
+})
+.catch(err => {
+  console.log(err)
+})
+  }, [])
+
   return (
     <>
     
--- frontend/src/Views/Home.jsx
+++ frontend/src/Views/Home.jsx
@@ -28,4 +28,6 @@ const Home = () => {
 
 
 
+
+
 export default Home
