179fe6ccca43078377b997f6dbea46ecfa47d56b
Rasmus <turboralle@hotmail.com>
2022-11-21 14:24:53 +0100

    :sparkles:  Added more backend related and added db to gitignore

---
+++ backendAPI/backendAPI/Controllers/PostsController.cs
@@ -0,0 +1,47 @@
+﻿using backendAPI.Data;
+using backendAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace backendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]  
+    public class PostsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public PostsController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetPosts()
+        {
+            return new OkObjectResult(await _context.Posts.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(PostRequest post)
+        {
+            try
+            {
+                var postEntity = new PostEntity
+                {
+                    Title = post.Title,
+                    Body = post.Body,
+                };
+                _context.Posts.Add(postEntity);
+                 await _context.SaveChangesAsync();
+                return new CreatedResult("", postEntity);
+            }
+            catch (Exception ex) 
+            { 
+                Debug.WriteLine(ex.Message);
+            }
+            return new BadRequestResult();
+
+            
+        }
+    }
+}
